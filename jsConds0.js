51322a8ab6010d6af2791331472b351bd7ad0625
Awienberg <a.wienberg@hotmail.com>
2019-09-03 12:41:32 +0200

    dice roll

---
+++ jsConds0.js
@@ -0,0 +1,216 @@
+//js.conds.0 dice roll
+'use strict';
+let r1 = 0;
+let r2 = 0;
+let r3 = 0;
+let r4 = 0;
+let r5 = 0;
+let r6 = 0;
+let roll;
+
+roll = Math.random();
+roll = Math.floor(roll * 6 + 1);
+if (roll === 1)
+    r1++;
+else if
+    (roll === 2)
+    r2++;
+else if
+    (roll === 3)
+    r3++;
+else if
+    (roll === 4)
+    r4++;
+else if
+    (roll === 5)
+    r5++;
+else if
+    (roll === 6)
+    r6++;
+
+roll = Math.random();
+roll = Math.floor(roll * 6 + 1);
+if (roll === 1)
+    r1++;
+else if
+    (roll === 2)
+    r2++;
+else if
+    (roll === 3)
+    r3++;
+else if
+    (roll === 4)
+    r4++;
+else if
+    (roll === 5)
+    r5++;
+else if
+    (roll === 6)
+    r6++;
+
+roll = Math.random();
+roll = Math.floor(roll * 6 + 1);
+if (roll === 1)
+    r1++;
+else if
+    (roll === 2)
+    r2++;
+else if
+    (roll === 3)
+    r3++;
+else if
+    (roll === 4)
+    r4++;
+else if
+    (roll === 5)
+    r5++;
+else if
+    (roll === 6)
+    r6++;
+
+roll = Math.random();
+roll = Math.floor(roll * 6 + 1);
+if (roll === 1)
+    r1++;
+else if
+    (roll === 2)
+    r2++;
+else if
+    (roll === 3)
+    r3++;
+else if
+    (roll === 4)
+    r4++;
+else if
+    (roll === 5)
+    r5++;
+else if
+    (roll === 6)
+    r6++;
+
+roll = Math.random();
+roll = Math.floor(roll * 6 + 1);
+if (roll === 1)
+    r1++;
+else if
+    (roll === 2)
+    r2++;
+else if
+    (roll === 3)
+    r3++;
+else if
+    (roll === 4)
+    r4++;
+else if
+    (roll === 5)
+    r5++;
+else if
+    (roll === 6)
+    r6++;
+
+roll = Math.random();
+roll = Math.floor(roll * 6 + 1);
+if (roll === 1)
+    r1++;
+else if
+    (roll === 2)
+    r2++;
+else if
+    (roll === 3)
+    r3++;
+else if
+    (roll === 4)
+    r4++;
+else if
+    (roll === 5)
+    r5++;
+else if
+    (roll === 6)
+    r6++;
+
+roll = Math.random();
+roll = Math.floor(roll * 6 + 1);
+if (roll === 1)
+    r1++;
+else if
+    (roll === 2)
+    r2++;
+else if
+    (roll === 3)
+    r3++;
+else if
+    (roll === 4)
+    r4++;
+else if
+    (roll === 5)
+    r5++;
+else if
+    (roll === 6)
+    r6++;
+
+roll = Math.random();
+roll = Math.floor(roll * 6 + 1);
+if (roll === 1)
+    r1++;
+else if
+    (roll === 2)
+    r2++;
+else if
+    (roll === 3)
+    r3++;
+else if
+    (roll === 4)
+    r4++;
+else if
+    (roll === 5)
+    r5++;
+else if
+    (roll === 6)
+    r6++;
+
+roll = Math.random();
+roll = Math.floor(roll * 6 + 1);
+if (roll === 1)
+    r1++;
+else if
+    (roll === 2)
+    r2++;
+else if
+    (roll === 3)
+    r3++;
+else if
+    (roll === 4)
+    r4++;
+else if
+    (roll === 5)
+    r5++;
+else if
+    (roll === 6)
+    r6++;
+
+roll = Math.random();
+roll = Math.floor(roll * 6 + 1);
+if (roll === 1)
+    r1++;
+else if
+    (roll === 2)
+    r2++;
+else if
+    (roll === 3)
+    r3++;
+else if
+    (roll === 4)
+    r4++;
+else if
+    (roll === 5)
+    r5++;
+else if
+    (roll === 6)
+    r6++;
+
+console.log('1: ' + r1);
+console.log('2: ' + r2);
+console.log('3: ' + r3);
+console.log('4: ' + r4);
+console.log('5: ' + r5);
+console.log('6: ' + r6);
